18d9311c54bef514db664ba36fa4ed8de2303b03
xiexupang <41959142+xiexupang@users.noreply.github.com>
2018-10-10 15:49:36 +0800

    Add files via upload

--- MNIST_CNN.py
+++ MNIST_CNN.py
@@ -13,12 +13,26 @@ LR = 0.001  # 学习率
 DOWNLOAD_MNIST = True  # True表明需要下载MNIST数据集
 
 
-# 下载训练神经网络的数据集
+# 下载MNIST数据集
 train_data = torchvision.datasets.MNIST(
     root='./mnist',
     train=True,
     transform=torchvision.transforms.ToTensor(),  # 把图片转化为tensor形式
     download=True,
 )
+#从MNIST中取出测试数据集
+test_data = torchvision.datasets.MNIST(
+    root='.mnist',
+    train=False,
+    download=True,
+)
+
+
+# 打印训练数据集的一张图片，看看它图片是怎样的
+print('train_data.size: ', train_data.train_labels.size())
+print('test_data.size: ', test_data.test_labels.size())
+plt.imshow(train_data.train_data[0])  # 展示第一张图
+plt.title('%i' % train_data.train_labels[0])
+plt.show()
 
 
